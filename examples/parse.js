05c56a1b9a80770ddc08bdab518ea9fa52b51871
taoyuan <torworx@gmail.com>
2016-03-23 11:49:34 +0800

    returns error if not recognized

--- examples/parse.js
+++ examples/parse.js
@@ -9,7 +9,7 @@ var parser = new Parser();
 var result;
 
 // receipt 1
-var buffer = require('./receipt1');
+var buffer = require('./fixtures/1');
 result = parser.parse(buffer);
 console.log('[receipt1]');
 console.log(result.commands);
@@ -32,7 +32,7 @@ console.log('****************************');
 
 // receipt 2
 
-result = parser.parse(require('./receipt2'));
+result = parser.parse(require('./fixtures/2'));
 console.log('[receipt2]');
 console.log(result.commands);
 console.log(iconv.decode(result.content, 'GB2312'));
